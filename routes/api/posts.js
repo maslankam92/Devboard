74ec8dc653ee44589a5df5adb9f7062854cac130
Marcin Maslanka <maslankam92@gmail.com>
2018-10-24 08:46:59 +0200

    Create route for get one post, get all posts and delete one post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -13,6 +13,31 @@ const validatePostInput = require("../../validation/post");
 */
 router.post("/", passport.authenticate("jwt", { session: false }), createPost);
 
+/*
+* @route GET api/posts
+* @description Gets all posts
+* @access Public
+*/
+router.get("/", getAllPosts);
+
+/*
+* @route GET api/posts/:id
+* @description Gets one post
+* @access Public
+*/
+router.get("/:id", getPost);
+
+/*
+* @route DELETE api/posts/:id
+* @description Deltes post
+* @access Public
+*/
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  deletePost
+);
+
 async function createPost(req, res) {
   const { errors, isValid } = validatePostInput(req.body);
 
@@ -27,6 +52,29 @@ async function createPost(req, res) {
 
   const savedPost = await newPost.save();
   return res.json(savedPost);
+}
+
+async function getAllPosts(req, res) {
+  const posts = await Post.find({}).sort({ date: -1 });
+  return res.json(posts);
+}
+
+async function getPost(req, res) {
+  const post = await Post.findById(req.params.id);
+  return res.json(post);
+}
+
+async function deletePost(req, res) {
+  const deletedPost = await Post.findById(req.params.id);
+
+  // Only owner can delete his post
+  if (deletedPost.user.toString() !== req.user.id) {
+    return res
+      .status(401)
+      .json({ notauthorized: "User is not authorized to delete this post" });
+  }
+  const postRemoved = await deletedPost.remove();
+  return res.json(postRemoved);
 }
 
 module.exports = router;
